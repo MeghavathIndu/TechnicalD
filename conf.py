7c69bd0608e4633a2f5a46ca550a0567fa289400
MeghavathIndu <79440914+MeghavathIndu@users.noreply.github.com>
2021-02-23 16:48:17 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'class.md'
+copyright = '2021,indu'
+author = 'indu'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
